075af431b3cc4684628ada7a61d46beed9cba9c0
Rose Lutz <rose.lutz.85@gmail.com>
2018-04-10 20:02:02 +0200

    Dealing with Adding contributors through selection box (select2 js)

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,4 +1,29 @@
 class MembershipsController < ApplicationController
   def index
+    @contributor = User.where(id:params[:contributors])
+  end
+
+  def new
+    @users = User.order(email: :asc).all
+  end
+
+  def create
+    @contributor = Membership.new(params[:contributor])
+    @room = Room.find(params[:room_id])
+
+    @contributor.room = @room
+    @contributor.user = current_user
+
+    if @contributor.save
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contributor_params
+    # params.require(:membership).permit(:status)
   end
 end
