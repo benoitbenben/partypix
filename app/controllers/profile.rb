89a7a99f44e6d1cb94f4d957d64b590ca1e3ca9a
benoit mathonat <benoit.mathonat@gmail.com>
2018-04-10 19:42:28 +0200

    add profile controller for edit avatar

---
+++ app/controllers/profile.rb
@@ -0,0 +1,20 @@
+class ProfileController < ApplicationController
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  # private
+
+  # def profile_params
+  #   params.require(:profile).permit(:nickname, :first_name, :last_name, :email, :avatar)
+  # end
+end
