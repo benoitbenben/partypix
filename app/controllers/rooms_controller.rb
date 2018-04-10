5bd0eb99a04f0bdb15bf5ca8e4c97a2874ee4eb3
benoit mathonat <benoit.mathonat@gmail.com>
2018-04-10 17:29:08 +0200

    add controller for rooms & views

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,30 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = current_user.all_rooms
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.user = current_user
+
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :date)
+  end
+end
