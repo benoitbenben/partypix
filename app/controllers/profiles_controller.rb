36bc9bfc464660d537185435b5a436c799e3f3df
benoit mathonat <benoit.mathonat@gmail.com>
2018-04-11 17:49:04 +0200

    add avatar to user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,26 @@ class ProfilesController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @profile = current_user
+
+  end
+
+  def update
+    @profile = current_user
+
+    if @profile.update(user_params)
+      redirect_to profile_path(@profile)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :first_name, :last_name, :email, :avatar)
+  end
+
 end
