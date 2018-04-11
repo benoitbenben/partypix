ace53e37f67907b5b9b94493ffc2f029d6a194e7
rose <35664575+RoseWagon6@users.noreply.github.com>
2018-04-11 13:54:47 +0200

    Update profiles_controller.rb
    limit the methods to the one implemented

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,27 +4,4 @@ class ProfilesController < ApplicationController
   def show
     @user = current_user
   end
-
-  # def create
-  #   @profile = User.find(profile_params)
-  #   # @user = User.find(params(new_user_registration))
-  #   if profile.save
-  #     redirect_to "My Profile", profile_path
-  #   else
-  #     render :new
-  # end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  private
-
-  def profile_params
-    params.require(:profile).permit(:nickname, :first_name, :last_name, :email, :avatar)
-  end
 end
