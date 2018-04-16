851ef7a263c7e5db8315d93483f787df002c24cf
benoit mathonat <benoit.mathonat@gmail.com>
2018-04-16 18:24:42 +0200

    metatags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name])
   end
-end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
+end
